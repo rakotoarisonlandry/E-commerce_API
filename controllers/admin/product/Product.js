61ba77ffea531a5ae8c81705e494688b1025a977
rakotorisonlandry <rakotorisonlandry@gmail.com>
2024-03-20 13:57:57 +0100

    update product

--- controllers/admin/product/Product.js
+++ controllers/admin/product/Product.js
@@ -1,34 +1,13 @@
 import { db } from "../../../config/bd.js";
-import nodemailer from "nodemailer"
-// export const addPro = (req, res) => {
-//   const q = "SELECT * FROM product WHERE  img =? ";
-//   db.query(q, [req.body.img], (err, data) => {
-//     if (err) return res.json(err);
-//     if (data.length) return res.status(409).json("product  already created!");
-//     const q =
-//       "INSERT INTO product (`NomPro`, `PrixInitial`,`PrixFinale`,`img`, `desc`, `date` ,`status`) VALUES (?)";
-//     const values = [
-//       req.body.NomPro,
-//       req.body.PrixInitial,
-//       req.body.PrixFinale,
-//       req.body.img,
-//       req.body.desc,
-//       req.body.date,
-//       req.body.status
-//     ];
-//     db.query(q, [values], (err, data) => {
-//       if (err) return res.status(500).json(err);
-//       return res.json("Product has been created !");
-//     });
-//   });
-// };
+import nodemailer from "nodemailer";
 export const addPro = (req, res) => {
   const q = "SELECT * FROM product WHERE img = ?";
   db.query(q, [req.body.img], (err, data) => {
     if (err) return res.json(err);
     if (data.length) return res.status(409).json("Product already created!");
 
-    const insertQuery = "INSERT INTO product (`NomPro`, `PrixInitial`,`PrixFinale`,`img`, `desc`, `date` ,`status`) VALUES (?)";
+    const insertQuery =
+      "INSERT INTO product (`NomPro`, `PrixInitial`,`PrixFinale`,`img`, `desc`, `date` ,`status`) VALUES (?)";
     const values = [
       req.body.NomPro,
       req.body.PrixInitial,
@@ -36,7 +15,7 @@ export const addPro = (req, res) => {
       req.body.img,
       req.body.desc,
       req.body.date,
-      req.body.status
+      req.body.status,
     ];
 
     db.query(insertQuery, [values], async (err, data) => {
@@ -48,31 +27,32 @@ export const addPro = (req, res) => {
         if (err) return res.status(500).json(err);
 
         // Construire la liste de destinataires
-        const recipients = customers.map(customer => customer.email);
-
+        const recipients = customers.map((customer) => customer.email);
         // Envoi d'e-mails simultanément
         const transporter = nodemailer.createTransport({
-          service: 'Gmail',
+          service: "Gmail",
           auth: {
-            user: 'rakotorisonlandry@gmail.com',
-            pass: "mpea dxeb unhw iewy "
-          }
+            user: "rakotorisonlandry@gmail.com",
+            pass: "mpea dxeb unhw iewy ",
+          },
         });
-        
+
         const mailOptions = {
-          from: 'rakotorisonlandry@gmail.com',
-          to: recipients.join(','), // Séparez les adresses par une virgule
-          subject: 'Nouveau produit disponible',
-          text: 'Bonjour, un nouveau produit est disponible. Venez le découvrir sur notre site : .aunion !'
+          from: "rakotorisonlandry@gmail.com",
+          to: recipients.join(","), // Séparez les adresses par une virgule
+          subject: "Nouveau produit disponible",
+          text: "Bonjour, un nouveau produit est disponible. Venez le découvrir sur notre site : .aunion !",
         };
 
         transporter.sendMail(mailOptions, (error, info) => {
           if (error) {
-            console.error('Error sending emails:', error);
+            console.error("Error sending emails:", error);
             return res.status(500).json("Failed to send emails.");
           } else {
-            console.log('Emails sent:', info.response);
-            return res.status(200).json("Product has been created and emails have been sent!");
+            console.log("Emails sent:", info.response);
+            return res
+              .status(200)
+              .json("Product has been created and emails have been sent!");
           }
         });
       });
